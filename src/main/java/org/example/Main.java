93739e9025113c1539308cc5663eae6a744a9537
Мария Галицкая <motyakoshka@gmail.com>
2023-03-22 16:28:15 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Мария";
+        System.out.println("Привет, меня зовут " + name);
     }
 }
